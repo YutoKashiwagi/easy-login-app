b4a4392cfd9f4c1c1a05e056c2dbe351f92f6d28
YutoKashiwagi <kashiwagi19950714@gmail.com>
2020-08-10 17:08:53 +0900

    ログイン周りのページ作成

---
+++ pages/authed-route.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h2>認証が必要なページ</h2>
+    <p>
+      cookieの'credential'の値がtrueな場合のみアクセス可能
+      <br />
+      <nuxt-link to="/">トップページへ</nuxt-link>
+    </p>
+  </div>
+</template>
